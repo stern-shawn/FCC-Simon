27d09263583a1fd8b32abadb1048c457fed61681
Shawn Stern <stern.shawn@gmail.com>
2016-08-03 15:48:09 -0700

    Updating html and js to make Strict Mode button have toggle-like visible behavior. CSS/JS/HTML run through linters to ensure quality and run through tidiers to ensure readibility and proper indentation. Finalized control logic for handling cases where user wins the game, makes correct moves, make mistakes while in strict/non-strict mode. Debugging console log statements cleaned out to make code lighter. Game technically complete at this point, only additional styling now!

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-// Generate random move Math.floor(Math.random()*4)
-
 // Initial game state
-var buttons = ['greenBtn','redBtn','yellowBtn','blueBtn'];
+var buttons = ['greenBtn', 'redBtn', 'yellowBtn', 'blueBtn'];
 var sequence = [];
 var counter = 0;
 var clickIndex = 0;
-var gameActive = false;
+var gameActive = false; // Flag for if game has begun or not
+var playing = false; // Flag for when actively animating a sequence, etc
 var strict = false;
 var winThreshold = 20;
 
-// Illuminate the given button for 500ms and return to original state
-// afterwards. Button is not interative for the duration.
+// Illuminate the given button for 500ms and return to original state afterwards. Button is not interative for the duration.
 // Input is expected to be the DOM object for a simonButton
 function lightButton(selected) {
   $(selected).addClass('unclickable');
-
   var lightColor = '';
   var btnIndex = 0;
-
-  console.log(selected.id);
 
   switch (selected.id) {
     case 'greenBtn':
@@ -39,18 +34,11 @@ function lightButton(selected) {
       break;
   }
 
-  // Lighten the button
+  // Lighten the button, play beep, and returned to dark state after 500ms
   $(selected).addClass(lightColor);
-  // Play the beep for this button
   playBeep(btnIndex);
-
-  // Note to self, 'this' within setTimeout refers to window by default, use a
-  // variable to store the correct 'this' and pass it!
-  // var that = this;
-  // Wait 500ms then return button to darkened state
   setTimeout(function() {
     $(selected).removeClass(lightColor).removeClass('unclickable');
-    console.log("Removing lighter colors");
   }, 500);
 }
 
@@ -60,7 +48,6 @@ function playBeep(selectedBtn) {
   var baseUrl = "https://s3.amazonaws.com/freecodecamp/";
   var audio = ["simonSound1.mp3", "simonSound2.mp3", "simonSound3.mp3", "simonSound4.mp3"];
   // Play corresponding audio by appending base url to selected audio file
-  console.log("Playing audio from: " + baseUrl + audio[selectedBtn]);
   new Audio(baseUrl + audio[selectedBtn]).play();
 }
 
@@ -76,8 +63,7 @@ function gameReset() {
 
 // Create and add a new move to the sequence
 function generateMove() {
-  var nextMove = Math.floor(Math.random()*4);
-  console.log("Making next move at index: " + nextMove);
+  var nextMove = Math.floor(Math.random() * 4);
   sequence.push(nextMove);
 }
 
@@ -85,29 +71,18 @@ function generateMove() {
 function playSequence() {
   // Create an interval which evaluates every 600ms (encapsulates the 500ms of the blink/audio)
   var interval = setInterval(function() {
-    // console.log(counter);
-    // console.log(sequence.length);
+    playing = true;
     if (counter < sequence.length) {
-      console.log("Playing sequence at: " + sequence[counter]);
       var currButton = $('#' + buttons[sequence[counter]]);
       // Index 0 of the returned object is the actual DOM object
       lightButton(currButton[0]);
       counter++;
     } else {
       counter = 0;
+      playing = false;
       clearInterval(interval);
     }
   }, 600);
-
-  // for (var i = 0; i < sequence.length; i++) {
-  //   console.log("Playing sequence at: " + sequence[i]);
-  //   var currButton = $('#' + buttons[sequence[i]]);
-  //   lightButton(currButton[0]);
-  //   // setTimeout(function() {
-  //   //   // Index 0 of the returned object is the actual DOM object
-  //   //   lightButton(currButton[0]);
-  //   // }, 500);
-  // }
 }
 
 $(document).ready(function() {
@@ -116,19 +91,6 @@ $(document).ready(function() {
     // Reset game
     gameReset();
     playSequence();
-    // Generate and do first move steps
-    // var firstMove = Math.floor(Math.random()*4);
-    // console.log("Starting game at index: " + firstMove);
-    // // Put this first value in the sequence
-    // sequence.push(firstMove);
-    // generateMove();
-    // Show the first move visibly to the user
-    // console.log(buttons[firstMove]);
-    // var firstButton = $('#' + buttons[sequence[0]]);
-    // Index 0 of the returned object is the actual DOM object
-    // console.log(firstButton[0]);
-    // lightButton(firstButton[0]);
-
     $('#count').html(sequence.length);
 
     setTimeout(function() {
@@ -136,100 +98,66 @@ $(document).ready(function() {
     }, 500);
   });
 
+  // Conditional logic for clicking a button at any point in the game
   $('.simonButton').click(function() {
-    console.log(this);
-    // console.log(sequence);
-    // console.log(sequence[clickIndex]);
-    // console.log(buttons[sequence[clickIndex]]);
-    // console.log(this.id);
-    // lightButton(this);
-
     // If user selects the correct button at this point in the sequence and has now won the game
-
+    if (gameActive && !playing && buttons[sequence[clickIndex]] === this.id && sequence.length === winThreshold && clickIndex === (winThreshold - 1)) {
+      gameActive = false;
+      lightButton(this);
+      setTimeout(function() {
+        alert("Congratulations! You've won by getting 20 correct in a row! Press 'start' to begin a new game...");
+      }, 550);
+    }
     // If user selects the correct button at this point in the sequence and at the end of the sequence
-    if (gameActive && buttons[sequence[clickIndex]] === this.id && (clickIndex + 1) === sequence.length) {
-      // alert("Correct!");
+    else if (gameActive && !playing && buttons[sequence[clickIndex]] === this.id && (clickIndex + 1) === sequence.length) {
       lightButton(this);
       // Generate a new move and reset player progress
       generateMove();
       clickIndex = 0;
 
       // Display the new sequence...
-      console.log("Positon in sequence should now be: " + clickIndex);
-      console.log("Number of moves/count is now: " + sequence.length);
-      console.log(sequence);
       $('#count').html(sequence.length);
       setTimeout(function() {
         playSequence();
-      }, 500);
-
+      }, 550);
     }
     // If user selects the correct button at this point in the sequence but isn't at the end of the sequence yet
-    else if (gameActive && buttons[sequence[clickIndex]] === this.id) {
+    else if (gameActive && !playing && buttons[sequence[clickIndex]] === this.id) {
       lightButton(this);
       clickIndex++;
-      console.log("Index: " + clickIndex);
-      console.log("Length: " + sequence.length);
     }
     // ... If user selects incorrect button while in normal mode
-    else if (gameActive && !strict) {
+    else if (gameActive && !playing && !strict) {
       lightButton(this);
-      alert("Oops! Close this box to view the correct order and try again!");
-      clickIndex = 0;
+      setTimeout(function() {
+        alert("Oops! Close this box to view the correct order and try again!");
+        clickIndex = 0;
+      }, 550);
       // Replay sequence here
       setTimeout(function() {
         playSequence();
-      }, 500);
+      }, 600);
     }
     // ... If user selects incorrect button while in strict mode
-    else if (gameActive && strict) {
+    else if (gameActive && !playing && strict) {
       lightButton(this);
-      alert("Oops! Strict mode is active, time to start from scratch!");
+      setTimeout(function() {
+        alert("Oops! Strict mode is active, time to start from scratch!");
+      }, 550);
       // Begin new game here
       gameReset();
       setTimeout(function() {
         playSequence();
-      }, 500);
+      }, 600);
     }
     // Otherwise game isn't active, have no response
     console.log(sequence);
-
-//     var currColor = '';
-//     var lightColor = '';
-
-//     switch (this.id) {
-//       case 'greenBtn':
-//         currColor = 'green';
-//         lightColor = 'greenLight';
-//         break;
-//       case 'redBtn':
-//         currColor = 'red';
-//         lightColor = 'redLight';
-//         break;
-//       case 'yellowBtn':
-//         currColor = 'yellow';
-//         lightColor = 'yellowLight';
-//         break;
-//       case 'blueBtn':
-//         currColor = 'blue';
-//         lightColor = 'blueLight';
-//         break;
-//     }
-
-//     // Lighten the button for 500ms then return to normal state
-//     $(this).addClass(lightColor);
-
-//     // Note to self, 'this' within setTimeout refers to window by default, use a variable to store the correct 'this' and pass it!
-//     var that = this;
-//     setTimeout(function() {
-//       $(that).removeClass(lightColor);
-//       console.log("Removing lighter colors");
-//     }, 500);
-   });
+  });
 
   // Toggle state of strict for now until we get a real toggle button created
   $('#strict').click(function() {
     strict = !strict;
+    strict ? $('#strict').addClass("btn-danger") : $('#strict').removeClass("btn-danger");
     console.log("Strict mode is now: " + strict);
   });
 });
