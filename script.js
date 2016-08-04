2e046160d22b8a9e6c3f41132208204f117bf450
Shawn Stern <stern.shawn@gmail.com>
2016-08-03 18:10:38 -0700

    Modified order of code to encapsulate everything within the document.ready statement, mainly to preload audio and try to, with some success, improve audio performance on mobile devices

--- script.js
+++ script.js
@@ -1,91 +1,114 @@
-// Initial game state
-var buttons = ['greenBtn', 'redBtn', 'yellowBtn', 'blueBtn'];
-var sequence = [];
-var counter = 0;
-var clickIndex = 0;
-var gameActive = false; // Flag for if game has begun or not
-var playing = false; // Flag for when actively animating a sequence, etc
-var strict = false;
-var winThreshold = 20;
+$(document).ready(function() {
+  // Try to preload audio to improve performance on mobile...
+  var greenAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+    redAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+    yellowAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+    blueAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
 
-// Illuminate the given button for 500ms and return to original state afterwards. Button is not interative for the duration.
-// Input is expected to be the DOM object for a simonButton
-function lightButton(selected) {
-  $(selected).addClass('unclickable');
-  var lightColor = '';
-  var btnIndex = 0;
+  // Initial game state
+  var buttons = ['greenBtn', 'redBtn', 'yellowBtn', 'blueBtn'];
+  var sequence = [];
+  var counter = 0;
+  var clickIndex = 0;
+  var gameActive = false; // Flag for if game has begun or not
+  var playing = false; // Flag for when actively animating a sequence, etc
+  var strict = false;
+  var winThreshold = 20;
 
-  switch (selected.id) {
-    case 'greenBtn':
-      lightColor = 'greenLight';
-      btnIndex = 0;
-      break;
-    case 'redBtn':
-      lightColor = 'redLight';
-      btnIndex = 1;
-      break;
-    case 'yellowBtn':
-      lightColor = 'yellowLight';
-      btnIndex = 2;
-      break;
-    case 'blueBtn':
-      lightColor = 'blueLight';
-      btnIndex = 3;
-      break;
+  // Illuminate the given button for 500ms and return to original state
+  // afterwards. Button is not interative for the duration.
+  // Input is expected to be the DOM object for a simonButton
+  function lightButton(selected) {
+    $(selected).addClass('unclickable');
+    var lightColor = '';
+    var btnIndex = 0;
+
+    switch (selected.id) {
+      case 'greenBtn':
+        lightColor = 'greenLight';
+        btnIndex = 0;
+        break;
+      case 'redBtn':
+        lightColor = 'redLight';
+        btnIndex = 1;
+        break;
+      case 'yellowBtn':
+        lightColor = 'yellowLight';
+        btnIndex = 2;
+        break;
+      case 'blueBtn':
+        lightColor = 'blueLight';
+        btnIndex = 3;
+        break;
+    }
+
+    // Lighten the button, play beep, and returned to dark state after 500ms
+    $(selected).addClass(lightColor);
+    playBeep(btnIndex);
+    setTimeout(function() {
+      $(selected).removeClass(lightColor).removeClass('unclickable');
+    }, 500);
   }
 
-  // Lighten the button, play beep, and returned to dark state after 500ms
-  $(selected).addClass(lightColor);
-  playBeep(btnIndex);
-  setTimeout(function() {
-    $(selected).removeClass(lightColor).removeClass('unclickable');
-  }, 500);
-}
-
-// Play a beep assosciated with each unique button
-// Input is expected to be an integer within range 0-3
-function playBeep(selectedBtn) {
-  var baseUrl = "https://s3.amazonaws.com/freecodecamp/";
-  var audio = ["simonSound1.mp3", "simonSound2.mp3", "simonSound3.mp3", "simonSound4.mp3"];
-  // Play corresponding audio by appending base url to selected audio file
-  new Audio(baseUrl + audio[selectedBtn]).play();
-}
-
-// Reset any existing simon sequences and counters to 0 and remove any visual
-// indicators of progress
-function gameReset() {
-  sequence = [];
-  $('#count').html(sequence.length);
-  clickIndex = 0;
-  gameActive = true;
-  generateMove();
-}
-
-// Create and add a new move to the sequence
-function generateMove() {
-  var nextMove = Math.floor(Math.random() * 4);
-  sequence.push(nextMove);
-}
-
-// Visually and audibly display the sequence to the player
-function playSequence() {
-  // Create an interval which evaluates every 600ms (encapsulates the 500ms of the blink/audio)
-  var interval = setInterval(function() {
-    playing = true;
-    if (counter < sequence.length) {
-      var currButton = $('#' + buttons[sequence[counter]]);
-      // Index 0 of the returned object is the actual DOM object
-      lightButton(currButton[0]);
-      counter++;
-    } else {
-      counter = 0;
-      playing = false;
-      clearInterval(interval);
+  // Play a beep assosciated with each unique button
+  // Input is expected to be an integer within range 0-3
+  function playBeep(selectedBtn) {
+    // var baseUrl = "https://s3.amazonaws.com/freecodecamp/";
+    // var audio = ["simonSound1.mp3", "simonSound2.mp3", "simonSound3.mp3", "simonSound4.mp3"];
+    // // Play corresponding audio by appending base url to selected audio file
+    // new Audio(baseUrl + audio[selectedBtn]).play();
+    // Modification to make this work with preloaded audio instead of instantiating a new audio
+    // object for each click (okay for desktops but way too slow on mobile)
+    switch (selectedBtn) {
+      case 0:
+        greenAudio.play();
+        break;
+      case 1:
+        redAudio.play();
+        break;
+      case 2:
+        yellowAudio.play();
+        break;
+      case 3:
+        blueAudio.play();
+        break;
     }
-  }, 600);
-}
+  }
 
-$(document).ready(function() {
+  // Reset any existing simon sequences and counters to 0 and remove any visual
+  // indicators of progress
+  function gameReset() {
+    sequence = [];
+    $('#count').html(sequence.length);
+    clickIndex = 0;
+    gameActive = true;
+    generateMove();
+  }
+
+  // Create and add a new move to the sequence
+  function generateMove() {
+    var nextMove = Math.floor(Math.random() * 4);
+    sequence.push(nextMove);
+  }
+
+  // Visually and audibly display the sequence to the player
+  function playSequence() {
+    // Create an interval which evaluates every 600ms (encapsulates the 500ms of the blink/audio)
+    var interval = setInterval(function() {
+      playing = true;
+      if (counter < sequence.length) {
+        var currButton = $('#' + buttons[sequence[counter]]);
+        // Index 0 of the returned object is the actual DOM object
+        lightButton(currButton[0]);
+        counter++;
+      } else {
+        counter = 0;
+        playing = false;
+        clearInterval(interval);
+      }
+    }, 600);
+  }
+
   $('#start').click(function() {
     $('#start').addClass('unclickable');
     // Reset game
